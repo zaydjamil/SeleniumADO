f63d3a3c076baec17139d006b886b99be23ba2da
Zayd Jamil <zaidjamil19@gmail.com>
2021-02-19 09:48:22 +0530

    Removed unused libraries

--- com.selenium.ado/src/test/java/com/ado/pipeline/BankGuru99.java
+++ com.selenium.ado/src/test/java/com/ado/pipeline/BankGuru99.java
@@ -3,12 +3,9 @@ package com.ado.pipeline;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.sun.tools.sjavac.Log;
 
 public class BankGuru99 {
 
